58ba88de27f2e498bcb4792ea39a15de4b4b0288
Dilip0552 <suthardilip.eis@gmail.com>
2025-08-11 14:06:27 +0530

    u

--- src/ManageClassesTab.jsx
+++ src/ManageClassesTab.jsx
@@ -214,7 +214,7 @@ function ManageClassesTab({ classes, addNotification }) {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
-            className="absolute inset-0 z-50 flex items-center justify-center bg-white bg-opacity-75"
+            className="inset-0 z-50 flex items-center justify-center bg-white bg-opacity-75"
           >
     <div className="w-full h-full flex flex-col items-start justify-start">
       <AnimatePresence>
